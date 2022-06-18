4667db05f2960d0346ddfdf0c8c1840d775dd625
Oliver George <oliver@condense.com.au>
2022-06-18 17:26:48 +1000

    establish some nss

---
+++ src/cnd_magus/pics_h.cljs
@@ -0,0 +1,4 @@
+(ns cnd-magus.pics-h)
+;void ReadPics( const char *name, word **pics, int maxPics, word *p );
+;void ErasePics( word **pics, int maxPics );
+;void PutPic( int x, int y, word *addr, long mode );
